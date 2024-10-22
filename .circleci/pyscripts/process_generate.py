ecc620962a8aee798d60e2e697b3f5e7fb105d71
umairk95 <umair.khalid@dataqhealth.com>
2024-10-22 14:22:47 +0500

    Improve python script in Circle CI

--- .circleci/pyscripts/process_generate.py
+++ .circleci/pyscripts/process_generate.py
@@ -257,7 +257,7 @@ if "trigger-build" in mappings:
                 ("docs" in combine_build_element  and "ui" in combine_build_element and "circleci_configuration" in combine_build_element  and len(check_build) == 3):
               del mappings["trigger-build"]
               check_build.clear()
-              del combine_build_element
+            #   del combine_build_element
               print("=========================>>>>>>>>",combine_build_element,len(check_build))
               break
             else:        
